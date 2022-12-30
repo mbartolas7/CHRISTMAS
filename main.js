bb1a627a3f194b27424e938ec638ddce43540bfd
Mathias6120 <93884797+mbartolas7@users.noreply.github.com>
2022-12-30 13:58:35 +0100

    30/12

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
+const input = document.getElementById("dateInput");
+
 function checkDate() {
-    const date = document.getElementById("dateInput").value
+    const date = input.value
     if(date == "06.05") {
         window.location.href = "./mainPage.html";
+    } else if(date == "") {
+        alert("Il faut écrire quelque chose !")
+    } else {
+        alert("C'est quoi ça ! Pas la bonne date !")
     }
 }
+
+input.addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+        checkDate()
+    }
+});
